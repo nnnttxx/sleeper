3dc1a0737945ff1ce4d7c1c2a3c5275accd2cd93
Benjamin Streck <benjamin.streck@hanning-hew.com>
2017-03-06 08:27:07 +0100

    Hotfix applied for timing bug
     - Watchdog is now disabled in WDT Interrupt
    
    WDT Interrupt is not disabled after use. If time awake is long enough and WDT expires after  instruction WDTCSR = _BV(WDCE) | _BV(WDE);  WDE is set and triggering a Watchdog System Reset. Due bootloader waittime WDE and WDRF cannot be cleared and the system is in an infite reset loop :(
    
    Recommended : disabling Interrupts during WDT setup and reset WDT before changing prescaler bits.

--- sleep.ino
+++ sleep.ino
@@ -2,18 +2,30 @@
 
 Sleeper g_sleeper;
 
+volatile unsigned int i;
+
 // Initialisation code
 void setup(void) {
-	Serial.begin(9600);
-	Serial.println("Setup done.");
+  Serial.begin(9600);
+  Serial.println("Setup done.");
+
+  Serial.print("Total milliseconds awake: ");
+  Serial.println(millis());
+  Serial.print("i: "); Serial.println(i);
+  delay(1000);
 }
 
 // Main loop
-void loop(void) {
-	Serial.print("Total milliseconds awake: ");
-    Serial.println(millis());
-    delay(100);
+void loop(void)
+{
+  // Power down for 10 seconds.
+  g_sleeper.SleepMillis(10000);
 
-	// Power down for 10 seconds.
-	g_sleeper.SleepMillis(10000);
+  while (1)
+  {
+    Serial.print("Total milliseconds awake: ");
+    Serial.println(millis());
+    Serial.print("i: "); Serial.println(i);
+    delay(1000);
+  }
 }
